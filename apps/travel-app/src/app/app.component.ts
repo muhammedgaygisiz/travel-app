a52308a11f59e89f52a7cad98b45032a4c247c83
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-02-01 11:25:44 +0100

    Add toolbar

--- apps/travel-app/src/app/app.component.ts
+++ apps/travel-app/src/app/app.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'travel-app-root',
-  template: '<p>Hello Moto</p>',
+  template: `
+    <mat-toolbar color="primary">
+      <button mat-icon-button>
+        <mat-icon>menu</mat-icon>
+      </button>
+      <span>Travel App</span>
+    </mat-toolbar>
+  `,
   styles: [],
 })
 export class AppComponent {
--- apps/travel-app/src/app/app.module.ts
+++ apps/travel-app/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -19,7 +22,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         registrationStrategy: 'registerWhenStable:30000'
       }
     ), 
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
 ],
   providers: [],
   bootstrap: [AppComponent],
