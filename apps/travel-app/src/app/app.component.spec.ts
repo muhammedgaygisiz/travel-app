fe36a24c6066997c5d1557820c891e38dbb49108
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-02-01 11:33:39 +0100

    Fix tests

--- apps/travel-app/src/app/app.component.spec.ts
+++ apps/travel-app/src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
+      imports: [MatToolbarModule, MatIconModule, MatButtonModule],
     }).compileComponents();
   });
 
