ccdf94cb9ff8aae7a70ef526dcc10521fb5afb1a
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-02-01 12:05:45 +0100

    add side nav

--- apps/travel-app/src/app/app.component.ts
+++ apps/travel-app/src/app/app.component.ts
@@ -3,14 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'travel-app-root',
   template: `
-    <mat-toolbar color="primary">
-      <button mat-icon-button>
-        <mat-icon>menu</mat-icon>
-      </button>
-      <span>Travel App</span>
-    </mat-toolbar>
+    <mat-sidenav-container hasBackdrop autosize>
+      <mat-sidenav mode="side" #sidenav>Drawer Content</mat-sidenav>
+      <mat-sidenav-content>
+        <mat-toolbar color="primary">
+          <button mat-icon-button (click)="sidenav.toggle()">
+            <mat-icon>menu</mat-icon>
+          </button>
+          <span>Travel App</span>
+        </mat-toolbar>
+      </mat-sidenav-content>
+    </mat-sidenav-container>
   `,
-  styles: [],
+  styles: [
+    `
+      .mat-sidenav-container {
+        height: 100vh;
+      }
+
+      .mat-sidenav {
+        padding: 20px;
+      }
+    `,
+  ],
 })
-export class AppComponent {
-}
+export class AppComponent {}
--- apps/travel-app/src/app/app.module.ts
+++ apps/travel-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,25 +9,22 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule, 
-    ServiceWorkerModule.register(
-      'ngsw-worker.js', 
-      {
-        enabled: environment.production,
-        // Register the ServiceWorker as soon as the app is stable
-        // or after 30 seconds (whichever comes first).
-        registrationStrategy: 'registerWhenStable:30000'
-      }
-    ), 
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
-],
+    MatButtonModule,
+    MatSidenavModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
