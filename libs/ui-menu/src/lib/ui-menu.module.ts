0c6cb0a210ed840dcd45cd0113a14c6ea05d759d
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2022-02-04 13:30:15 +0100

    Add component to ui-menu library

--- libs/ui-menu/src/lib/ui-menu.module.ts
+++ libs/ui-menu/src/lib/ui-menu.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    MenuComponent
+  ],
+  exports: [
+    MenuComponent
+  ],
 })
 export class UiMenuModule {}
